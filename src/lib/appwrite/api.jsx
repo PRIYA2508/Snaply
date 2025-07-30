07ea2463dc8cad1ac9809a939b8fc38b55d90c43
ps3869253@gmail.com <ps3869253@gmail.com>
2025-07-30 18:40:30 +0530

    appwrite

--- src/lib/appwrite/api.jsx
+++ src/lib/appwrite/api.jsx
@@ -1,4 +1,9 @@
 //this function is used for the first time to get the user's input
-export async function createUserAccount( ){
+export async function createUserAccount(user){
+try{
 
+}catch(error){
+    console.log("Error creating user account")
+    return error 
+}
 }
